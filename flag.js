afab92683e175f94a94f515c32c73820ff5a3768
Fizam.dev <crawlernight548@gmail.com>
2026-01-18 16:39:43 +0800

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,9 +1,8 @@
-function check() {
-  var _0x1a2b = "fXieXSF_7nEmele_3hTh_dn1h3B{FTCDH";
-  var decoded = _0x1a2b.split("").reverse().join("");
-  
-  if (window.location.search.includes("debug=true")) {
-    console.log("Flag: " + decoded);
-  }
+var _data = ["SERSVEZ7QjNoMW5kX1RoM18zbGVtRW43XyFeIX0="];
+
+function init() {
+    var secret = atob(_data[0]);
+    window.secretFlag = secret;
 }
-check();
+
+init();
